6a4c41743240af99615e54f97944776ccfc32e04
kirill <kirill.titov@outlook.com>
2020-05-07 01:40:02 +0300

    init lab_01

---
+++ lab_01/lab_01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_01
+//
+//  Created by Кирилл Титов on 07.05.2020.
+//  Copyright © 2020 Кирилл Титов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
